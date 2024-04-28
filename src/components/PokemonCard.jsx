de7f9ff40d59c4e8a3ee6e149f9fa8e74a760475
Vanessa <nessa_g@hotmail.fr>
2024-04-28 23:36:41 +0200

    Quete 4

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,14 +1,12 @@
-function PokemonCard() {
-  const PokemonCard = ({ pokemon }) => {
-
+const PokemonCard = ({ pokemon }) => {
   return (
+    <>
     <figure>
-    {pokemon.imgSrc !== undefined ? <img src={pokemon.imgSrc} alt={pokemon.name} /> : 
-    <p>???</p>}
+      {pokemon.imgSrc !== undefined ? <img src={pokemon.imgSrc} alt={pokemon.name}></img> : <p>???????</p>}
+      <figcaption>{pokemon.name}</figcaption>
     </figure>
-    );
-  }
-
-
+    </>
+  )
 }
-export default PokemonCard;
+
+export default PokemonCard
